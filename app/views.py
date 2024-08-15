a3d8743823f7292cdb845e4b8846ad3fe2ecc7e7
sanny gupta <sannygupta79@gmail.com>
2024-08-15 13:08:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def patna(request):
+    return HttpResponse('data that we have to respond')
+
+# Create your views here.
